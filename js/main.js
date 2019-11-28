4450ece91cd3537731b6d012eac901be952fa765
lauraherrero <lauraherrero36@gmail.com>
2019-11-28 10:12:52 +0100

    Change functions in the js: create a main function event handler and other for increase attemps and check user number. Add css styles

--- js/main.js
+++ js/main.js
@@ -3,47 +3,45 @@
 const elementInputNumber = document.querySelector('#input-number');
 const elementButton = document.querySelector('#btn');
 const elementTextClue = document.querySelector('#clue');
-const elementTextTry = document.querySelector('#game-number');
+const elementTextTry = document.querySelector('#count-number');
 let attemps = 0;
+const maxNumber = 100;
 
-
+//Creo una función para que me genere un número aleatorio con valor máximo 100.
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
-  
+
+}
+//Guardo el número aleatorio generado en una constante que usaré después para checkear con el número del input.
+const randomNumber = getRandomNumber(maxNumber);
+
+//Creo una función que me incremente el número de intentos para mi contador.
+const increaseAttemps = () => {
+  attemps++;
+  elementTextTry.innerHTML = 'Tú número de intentos es: ' + `${attemps}`;
 }
 
-function increaseAttemps() {
-  if(attemps < 1){
-    attemps++;
+//Creo una función que va a comparar el valor del input con el número generado aleatoriamente. Dependiendo del valor, me dará unas pistas u otras para saber si estoy por encima, por debajo, el valor no es correcto o si se ha acertado
+const checkUserNumber = () => {
+  if (elementInputNumber.value > maxNumber || elementInputNumber.value === '' || elementInputNumber.value <=0){
+    elementTextClue.innerHTML = 'Tu número debe de estar entre 1 y 100';
+  } else if (elementInputNumber.value > randomNumber) {
+    elementTextClue.innerHTML = 'Tu número es demasiado alto';
+  } else if (elementInputNumber.value < randomNumber) {
+    elementTextClue.innerHTML = 'Tu número es demasiado bajo';
+  } else {
+    elementTextClue.innerHTML = 'Has ganado campeona!!!!!!';
   }
-  elementTextTry.innerHTML = 'Tú número de intentos es: ' + `${attemps++}`;
 }
 
-const randomNumber = getRandomNumber(100);
+//Muestro en mi consola el número aleatorio
 console.log(randomNumber);
 
-function tryIt () {
-  console.log(elementInputNumber.value);
-  console.log(getRandomNumber(100));
-  
-  compare (randomNumber);
+//Creo mi función manejadora que funciona con el evento del click en el botón y que contiene las funciones que aumentan mi contador y realizan el checkeo.
+function getClickHandler () {
   increaseAttemps ();
-
-
-  function compare (num) {
-
-    if (elementInputNumber.value > randomNumber){
-      elementTextClue.innerHTML = 'Demasiado alto';
-    } else if (elementInputNumber < randomNumber){
-      elementTextClue.innerHTML = 'Demasiado bajo';
-    } else if (elementInputNumber === randomNumber){
-      elementTextClue.innerHTML = 'Has ganado campeona!!!!';
-    } else {elementTextClue.innerHTML = 'El número debe estar entre 1 y 100';
-    }
-  }
+  checkUserNumber ();
 }
- 
 
 
-
-elementButton.addEventListener('click', tryIt);
+elementButton.addEventListener('click', getClickHandler);
